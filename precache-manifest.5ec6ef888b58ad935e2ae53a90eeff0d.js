cdd5cd0f9730e3787e1d36d867ea41221addd7ae
mistervaibhav <vebatlow2@gmail.com>
2020-05-03 01:47:37 +0530

    Updates

---
+++ precache-manifest.5ec6ef888b58ad935e2ae53a90eeff0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620738e7063785ff72b73d1accf074af",
+    "url": "/react-random-advice-app/index.html"
+  },
+  {
+    "revision": "d196aee4d824106ca4ec",
+    "url": "/react-random-advice-app/static/css/main.2770e11b.chunk.css"
+  },
+  {
+    "revision": "c26ba28a2da3d8d486c9",
+    "url": "/react-random-advice-app/static/js/2.76e45c7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-random-advice-app/static/js/2.76e45c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d196aee4d824106ca4ec",
+    "url": "/react-random-advice-app/static/js/main.acda0a61.chunk.js"
+  },
+  {
+    "revision": "6ce1f507f09deaa3c65f",
+    "url": "/react-random-advice-app/static/js/runtime-main.a4962ea5.js"
+  }
+]);
